4791d4096ed0e3871d3516fad6dfa6a8217ffd04
CarlosGo1994 <charls21ronald@gmail.com>
2020-02-22 19:08:34 -0600

    Differents sentences how can show messages to the user

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//alert
+alert("This is my alert");
+alert("This is my text");
+
+//Confirmation
+
+var my_result = confirm("Are you sure tha you want to continue");
+console.log(my_result);
+
+//Data Input
+var my_result1 = prompt("How old are you?", 18);
+console.log(my_result1);
